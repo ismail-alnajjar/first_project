17a1bd3863692b44cb2c9ad75669033b8965ff63
ismail-alnajjar <ismaillalnajjaar@gmail.com>
2025-05-27 12:38:06 +0300

    +1

--- first_project/lib/main.dart
+++ first_project/lib/main.dart
@@ -104,7 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('Ismail alnajjar1:'),
+            const Text('Ismail alnajjar شمتتت1:'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
